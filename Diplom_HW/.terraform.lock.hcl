3821e075814f802fe262add87fb912124bbedd25
AndreiBakaushyn <andrei.bakaushyn@gmail.com>
2025-01-29 11:03:34 +0300

    Diplom

---
+++ Diplom_HW/.terraform.lock.hcl
@@ -0,0 +1,42 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "5.84.0"
+  constraints = ">= 5.46.0, >= 5.49.0, >= 5.81.0"
+  hashes = [
+    "h1:dwpeFUdcxgXVAc0JSqO57xf0/r2qOBLPloombCQWFz8=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/cloudinit" {
+  version     = "2.3.5"
+  constraints = ">= 2.0.0"
+  hashes = [
+    "h1:HCoabXm6NQwCivl1q24+l9VUufc2mFqNeulsQBA9iFg=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version     = "3.2.3"
+  constraints = ">= 3.0.0"
+  hashes = [
+    "h1:+AnORRgFbRO6qqcfaQyeX80W0eX3VmjadjnUFUJTiXo=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version     = "0.12.1"
+  constraints = ">= 0.9.0"
+  hashes = [
+    "h1:6BhxSYBJdBBKyuqatOGkuPKVenfx6UmLdiI13Pb3his=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version     = "4.0.6"
+  constraints = ">= 3.0.0"
+  hashes = [
+    "h1:dYSb3V94K5dDMtrBRLPzBpkMTPn+3cXZ/kIJdtFL+2M=",
+  ]
+}
---
+++ Diplom_HW/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  backend "s3" {
+    bucket         = "andreibakaushyn-terraform-state-bucket"
+    key            = "eks-cluster/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "andreibakaushyn-terraform-lock-table"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "./vpc-module" # Локальный путь к клонированному модулю VPC
+
+  name             = "andreibakaushyn-eks-vpc"
+  cidr             = "10.0.0.0/16"
+  azs              = ["us-east-1a", "us-east-1b"]
+  public_subnets   = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Owner       = "andreibakaushyn"
+    Environment = "dev"
+  }
+}
+
+resource "aws_ecr_repository" "repo" {
+  name = "andreibakaushyn-eks-repo"
+}
+
+module "kms" {
+  source = "./kms-module"
+  
+  create = true
+  description = "KMS key for EKS cluster"
+  tags = {
+    Environment = "dev"
+    Owner       = "andreibakaushyn"
+  }
+}
+
+module "eks" {
+  source = "./eks-module" # Локальный путь к модулю EKS
+
+  cluster_name    = "andreibakaushyn-eks-cluster"
+  cluster_version = "1.27"
+  vpc_id          = module.vpc.vpc_id #Передача ID VPC
+
+  # Используем корректные переменные из модуля
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 2
+      min_capacity     = 1
+
+      instance_type = "t3.micro"
+    }
+  }
+  
+}
